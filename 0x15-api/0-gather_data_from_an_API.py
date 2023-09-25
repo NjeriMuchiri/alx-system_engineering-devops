e09f22ec5d1c612c61f81a142b8812ce2b860e1a
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-25 21:38:36 +0300

    employees fetch data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This script uses JSONPlaceholder API to get information about employee ID"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = '{}users/{}'.format(url, sys.argv[1])
+    response = requests.get(user)
+    json_obj = response.json()
+    print("Employee {} is done with tasks".format(json_obj.get('name')), end="")
+
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    response = requests.get(todos)
+    tasks = response.json()
+    task_to = []
+    for task in tasks:
+        if task.get('completed') is True:
+            task_to.append(task)
+
+    print("({}/{}):".format(len(task_to), len(tasks)))
+    for task in task_to:
+        print("\t {}".format(task.get("title")))
