ed3fe2b67ec86e1e05dc897ea84d8ac1a58292ca
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-10-10 22:14:13 +0300

    puppet fixation

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+# Fixes 500 error in the wordpress file wp-settngs.php
 
 exec {'replace':
-  provider => shell,
+  path     => '/usr/local/bin/:/bin/',
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
