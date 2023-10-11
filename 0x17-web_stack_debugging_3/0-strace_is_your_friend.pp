dc4aa676403ed9076a8f600a01eb35ca1ac9c56d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-10-11 14:40:51 +0300

    puppet fixation

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes 500 error in the wordpress file wp-settngs.php
 
 exec {'wordpress-fix':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path     => '/usr/local/bin/:/bin/'
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
