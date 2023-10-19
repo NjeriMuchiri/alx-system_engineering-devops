e6bdbabcf2746782668893da63d2af9b07a61117
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-10-19 11:57:41 +0300

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,13 +2,12 @@
 
 exec {'replace-2':
   provider => shell,
-  command  => 'sudo sed -i "s/holberton /5/50000/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/holberton hard/5/50000/" /etc/security/limits.conf',
   path     => '/usr/local/bin/:/bin/',
   before   => Exec['replace-2'],
 }
 
 exec {'replace-2':
   provider => shell,
-  command  => 'sudo sed -i "s/holberton /4/50000/" /etc/security/limits.conf',
-  path     => '/usr/local/bin/:/bin/',
+  command  => 'sudo sed -i "s/holberton soft/4/50000/" /etc/security/limits.conf',
 }
