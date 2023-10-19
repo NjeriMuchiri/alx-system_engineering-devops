c18b685700fca41a954b47ac729ba3d12335cdbb
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-10-19 11:39:53 +0300

    limit user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -9,6 +9,6 @@ exec {'replace-2':
 
 exec {'replace-2':
   provider => shell,
-  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 4/nofile 50000/" /etc/security/limits.conf',
   path     => '/usr/local/bin/:/bin/',
 }
