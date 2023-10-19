d8d5f0977a30d02839225aee9fabc9d68a37b4cf
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-10-19 23:32:27 +0300

    limits user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,13 @@
 # Fixing login whereby user can open a file without error message
 
-exec { 'adding-hard-file-limit-for-holberton-user':
+exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/65000/" /etc/security/limits.conf',
-  before   => Exec['adding-hard-file-limit-for-holberton-user'],
+  before   => Exec['increase-hard-file-limit-for-holberton-user'],
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'adding-soft-file-limit-for-holberton-user':
+exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/65000/" /etc/security/limits.conf',
+  before   => Exec['increase-soft-file-limit-for-holberton-user'],
   path    => '/usr/local/bin/:/bin/'
 }
